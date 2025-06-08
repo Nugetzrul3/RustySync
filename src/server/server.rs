9e7d6ada3b68cfc17ab2149ae00506a53055933b
Nugetzrul3 <popingcorn12@gmail.com>
2025-06-09 01:33:00 +1000

    Added initial code for web server - Created actix API and added test route

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,1 +1,22 @@
 // Main logic for hosting Actix-Web HTTP server
+use actix_web::{ web, App, HttpServer, HttpResponse, Responder };
+use serde_json::json;
+
+// basic server health check route
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().json(
+        json!({
+            "status": "ok",
+        })
+    )
+}
+// main server startup
+pub async fn start(port: u16) -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health_check))
+    })
+        .bind(("127.0.0.1", port))?
+        .run()
+        .await
+}
